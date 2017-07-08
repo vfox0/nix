d9d5f042f72e524fc66b8f2c0bf9859e7840eab8
JustDidier <creatorhxn@gmail.com>
2017-07-08 14:51:03 +0200

    Add files via upload

---
+++ NIX1.bat
@@ -0,0 +1,5 @@
+@echo off
+
+echo Hey User This is gonna be a big project!
+pause
+exit
---
+++ NIX5.bat
@@ -0,0 +1,13 @@
+@echo off
+title NIX Save Launcher...
+cd C:\Users\Wilma\AppData\Roaming\Spotify
+echo Loading...
+timeout 2 > NUL
+echo Done..!
+cls
+echo Starting...
+timeout 5 > NUL
+echo Done..!
+start spotify
+timeout 1 > NUL
+exit
